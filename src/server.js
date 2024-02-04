0ece427ea46251d7894ab1774131a4349ca72736
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-02-04 18:10:29 +0900

    3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
@@ -13,7 +13,8 @@ const handleHome = (req, res) => {
   return res.send('I love JS');
 };
 
-app.get('/', gossipMiddleware, handleHome);
+app.use(logger);
+app.get('/', handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
